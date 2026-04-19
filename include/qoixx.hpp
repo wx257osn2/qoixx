fa4479d6a1258f561098556adb1a6fafa3620af1
I <1091761+wx257osn2@users.noreply.github.com>
2026-04-20 00:26:25 +0900

    remove ub

--- include/qoixx.hpp
+++ include/qoixx.hpp
@@ -758,7 +758,7 @@ class qoi{
     0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
   };
   static constexpr unsigned lsb32(std::uint32_t x)noexcept{
-    return de_bruijn_bit_position_sequence[(static_cast<std::uint32_t>(x&-static_cast<std::int32_t>(x))*0x077cb531u) >> 27];
+    return de_bruijn_bit_position_sequence[(static_cast<std::uint32_t>(x&(~x+1u))*0x077cb531u) >> 27u];
   }
   template<std::uint8_t M>
   static inline __m256i slli_epi8(__m256i v)noexcept{
